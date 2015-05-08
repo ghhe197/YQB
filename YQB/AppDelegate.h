fe24d16b913b64bb059d2a7fded5853200969fb8
jian <jian@zuoquans-iMac.local>
2015-05-08 15:13:15 +0800

    Initial Commit

---
+++ YQB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YQB
+//
+//  Created by jian on 15/5/8.
+//  Copyright (c) 2015年 jian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YQB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YQB
+//
+//  Created by jian on 15/5/8.
+//  Copyright (c) 2015年 jian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
